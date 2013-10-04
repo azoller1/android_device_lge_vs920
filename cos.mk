1e1f076f4094267352b2a8b4329aeb9031db1c07
Alex <alexzoller1@gmail.com>
2013-10-04 18:06:39 -0500

    removed selinux table, files changed to work with chamos, apns updated

--- cos.mk
+++ cos.mk
@@ -3,24 +3,24 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/config/apns-conf.xml:system/etc/apns-conf.xml
 
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/cdma.mk)
+$(call inherit-product, vendor/cos/config/cdma.mk)
 
 TARGET_BOOTANIMATION_NAME := vertical-720x1280
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cos/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/vs920/vs920.mk)
 
-PRODUCT_NAME := cm_vs920
+PRODUCT_NAME := cos_vs920
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := Spectrum
 PRODUCT_VERSION_DEVICE_SPECIFIC :=
 
 ## Device identifier. This must come after all inclusions
-TARGET_PRODUCT := cm_vs920
+TARGET_PRODUCT := cos_vs920
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=i_vzw TARGET_DEVICE=i_vzw BUILD_FINGERPRINT=Verizon/i_vzw/i_vzw:4.0.4/IMM76D/VS920ZV7.47e5065f:user PRIVATE_BUILD_DESC="i_vzw-user 4.0.4 IMM76D 47e5065f release-keys"
 
